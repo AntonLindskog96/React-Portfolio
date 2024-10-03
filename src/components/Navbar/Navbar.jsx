6776a188820eb329bdcf02de4ea6134070d7a021
Anton Lindskog <lindskog96@live.se>
2024-10-03 13:08:12 +0200

    responsive, navfixtures

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,9 +1,46 @@
-import React from 'react'
+import React, { useState } from "react";
+
+import styles from "./Navbar.module.css";
+import { getImageUrl } from "../../utils";
 
 const Navbar = () => {
-  return <nav>
-    <a href='/'>portfolio</a>
-  </nav>
-}
+  const [menuOpen, setMenuOpen] = useState(false);
+  return (
+    <nav className={styles.navbar}>
+      <a className={styles.title} href="/">
+        portfolio
+      </a>
+      <div className={styles.menu}>
+        <img
+          className={styles.menuBtn}
+          src={
+            menuOpen
+              ? getImageUrl("nav/closeIcon.png")
+              : getImageUrl("nav/menuIcon.png")
+          }
+          alt="menu-button"
+          onClick={() => setMenuOpen(!menuOpen)}
+        />
+        <ul
+          className={`${styles.menuItems} : ${menuOpen && styles.menuOpen}`}
+          onClick={() => setMenuOpen(false)}
+        >
+          <li>
+            <a href="#about">About</a>
+          </li>
+          <li>
+            <a href="#experience">Experience</a>
+          </li>
+          <li>
+            <a href="#project">Projects</a>
+          </li>
+          <li>
+            <a href="#contact">Contact</a>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  );
+};
 
-export default Navbar
+export default Navbar;
--- src/utils.js
+++ src/utils.js
@@ -1,3 +1,3 @@
-export const getImageUrl = {path} => {
-    return new URL(`assets/${path}`, import.meta.URL).href;
+export const getImageUrl = (path) => {
+    return new URL(`/assets/${path}`, import.meta.url).href;
 }
